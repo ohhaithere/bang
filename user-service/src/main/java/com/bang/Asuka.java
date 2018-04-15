ffc2672deef2c04f6714d6bf3e2f3d47bb96c07a
frostymaster <frostmasterwork@gmail.com>
2018-04-15 03:22:44 +0300

    Add eureka clients

--- user-service/src/main/java/com/bang/Asuka.java
+++ user-service/src/main/java/com/bang/Asuka.java
@@ -2,11 +2,13 @@ package com.bang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by johnnyGrimes on 23/02/2018.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Asuka {
 
     public static void main(String[] args) {
